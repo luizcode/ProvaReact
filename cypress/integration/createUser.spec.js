9f7f6664ff2272d02e56cf96a9bde120ebe714a2
luizcode <luizfernandovv043@gmail.com.br>
2021-12-09 16:40:58 -0400

    feat: create test for a new bet

--- cypress/integration/createUser.spec.js
+++ cypress/integration/createUser.spec.js
@@ -2,7 +2,8 @@
 
 describe('App.Bet', () => {
     it('Fazer cadastro de usuario', () => {
-        cy.visit("http://localhost:3000/registration");
+        cy.visit("http://localhost:3000/");
+        cy.get('.gYoDgS').click();
         cy.get(':nth-child(1) > .sc-pVTFL').type("admin");
         cy.get(':nth-child(2) > .sc-pVTFL').type("admin@admin.com");
         cy.get(':nth-child(3) > .sc-pVTFL').type("admin123");
@@ -15,6 +16,43 @@ describe('App.Bet', () => {
         cy.get(':nth-child(2) > .sc-pVTFL').type("admin123");
         cy.get('.sc-eCImPb > :nth-child(2)').click();
     });
+
+    it("Fazer aposta de mega", () => {
+        cy.get('.sc-bqiRlB').click();
+        cy.get('.khnSrR').click();
+        Cypress._.times(3, () => {
+            cy.get('.sc-AjmGg > :nth-child(1) > :nth-child(1)').click();
+            cy.get('.sc-hUpaCq').click();
+        })
+    })
+
+    it("Fazer aposta da lotofacil", () => {
+        cy.get('.lePWaS').click();
+        Cypress._.times(3, () => {
+            cy.get('.sc-AjmGg > :nth-child(1) > :nth-child(1)').click();
+            cy.get('.sc-hUpaCq').click();
+        })
+    })
+
+    it("Fazer aposta da quina", () => {
+        cy.get('.eLVxCv').click();
+        Cypress._.times(3, () => {
+            cy.get('.sc-AjmGg > :nth-child(1) > :nth-child(1)').click();
+            cy.get('.sc-hUpaCq').click();
+        })
+    })
+
+    it("Fazer aposta do jogo do bixo", () => {
+        cy.get('.cRLrbx').click();
+        Cypress._.times(3, () => {
+            cy.get('.sc-AjmGg > :nth-child(1) > :nth-child(1)').click();
+            cy.get('.sc-hUpaCq').click();
+        })
+    })
+
+    it("Salvar o game", () => {
+        cy.get('.sc-dJjYzT').click();
+    })
 
 
 });
